1a903425d331b6fef4ee52604b2e6f1c896b6fdb
gabriel ferri <107404209+ferrigb@users.noreply.github.com>
2024-04-15 08:50:34 -0300

    coloquei imagem

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function verificarTime() {
     var respostaTime = prompt("Qual o melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "Palmeiras") {
+      alert("Isso mesmo! O Palmeiras é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
